7454c4c7481808727d9ec28cf6b4e2a6daf761ba
Essu-man <essumanconstance514@gmail.com>
2024-10-25 14:06:56 +0000

    page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-const logoUrl = "https://firebasestorage.googleapis.com/v0/b/medcandetect.appspot.com/o/dvla%20logo.jpeg?alt=media&token=9da938bd-45d2-4107-a1de-357eccf600b0";
-
+const logoUrl ="https://firebasestorage.googleapis.com/v0/b/medcandetect.appspot.com/o/logo.jpg?alt=media&token=ffa9b8be-d4c4-4542-9b4e-bcf23a043c0c";
 function SignIn() {
   return (
     <div style={styles.container}>
-      <img src={logoUrl} alt="Your Company Logo" style={styles.logo} />
-      <h2 style={styles.title}>Sign in to your account</h2>
+      <img src={logoUrl} alt="" style={styles.logo} />
+
 
       <form style={styles.form}>
         <div style={styles.formGroup}>
@@ -17,7 +16,7 @@ function SignIn() {
           <input type="password" id="password" name="password" required style={styles.input} />
         </div>
 
-        <button type="submit" style={styles.button}>Sign in</button>
+        <button type="submit" style={styles.button}>Login</button>
       </form>
     </div>
   );
@@ -26,14 +25,15 @@ function SignIn() {
 
 const styles = {
   container: {
-    maxWidth: '400px',
+    maxWidth: '600px',
     width: '100%',
     padding: '20px',
-    margin: '0 auto',
+    margin: '50px auto ',
     backgroundColor: '#04A84A',
-    borderRadius: '8',
+    borderRadius: '10px',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
     textAlign: 'center',
+    marginBottom:'90px',
   },
   logo: {
     height: '100px',
@@ -42,7 +42,7 @@ const styles = {
   },
   title: {
     fontSize: '1.5rem',
-    color: '#111827',
+    color: '#000',
     marginTop: '20px',
   },
   form: {
@@ -54,19 +54,19 @@ const styles = {
   },
   label: {
     fontSize: '0.875rem',
-    color: '#374151',
+    color: '#000',
   },
   input: {
-    width: '100%',
+    width: '50%',
     padding: '10px',
     marginTop: '5px',
     border: '1px solid #d1d5db',
-    borderRadius: '4px',
+    borderRadius: '24px',
     fontSize: '0.875rem',
     color: '#111827',
   },
   button: {
-    width: '100%',
+    width: '30%',
     padding: '10px',
     marginTop: '20px',
     backgroundColor: '#000',
@@ -74,7 +74,7 @@ const styles = {
     fontSize: '0.875rem',
     fontWeight: 'bold',
     border: 'none',
-    borderRadius: '4px',
+    borderRadius: '30px',
     cursor: 'pointer',
   },
   buttonHover: {
