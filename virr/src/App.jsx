3b0cc6015f1ce6136b1306c9e3df77c6d515fb10
Essu-man <essumanconstance514@gmail.com>
2024-10-27 19:10:43 +0000

    Commit

---
+++ virr/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import SignIn from './Hero'; // Adjust this path if necessary
+
+function App() {
+  return (
+    <div className="app-container">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
